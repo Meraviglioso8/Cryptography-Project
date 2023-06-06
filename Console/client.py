9abb7b8a77f7dde6d18e52731fd55797b803284b
Huyen Tran <46748862+Meraviglioso8@users.noreply.github.com>
2023-06-06 22:33:18 +0700

    update new OTP + new file name

--- Console/client.py
+++ Console/client.py
@@ -1,32 +1,106 @@
 import socket
+import ssl
 import threading
+import requests
+import hmac
+import hashlib
+import struct
+import time
+from binascii import hexlify
+from getpass import getpass
 
+# SSL context
+context = ssl.create_default_context(ssl.Purpose.SERVER_AUTH)
+context.load_verify_locations(cafile="server.crt")
+context.verify_mode = ssl.CERT_REQUIRED
+
+# Connect to the server
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR, 1)
+client = context.wrap_socket(client, server_hostname="Group6")
 client.connect(("localhost", 9999))
-print("Client connecting...")
+
+print("Client connected successfully")
 def receive():
     while True:
         try:
-            rev = client.recv(1024).decode()
-            print(rev)
-        except:
-            print('Error! Cannot receive from server!')
+            message = client.recv(1024)
+            if message.startswith(b"FACTOR:"):
+                # Received the encrypted factor, save it to file
+                factor = message[-32:]
+                username = message.decode().split(':')[1].split('/')[0]
+                filename = hashlib.sha256(username.encode()).hexdigest()
+                print
+                with open(str(filename[:10]), "wb") as f:
+                    f.write(bytes.fromhex(factor.decode()))
+                print("factor saved to file. input OK for confirmation")
+            else:
+                # Received a regular message, print it to the console
+                print(message.decode())
+        except Exception as e:
+            print(e)
             client.close()
             break
-
+def reqOTP(username):
+    filename = hashlib.sha256(username.encode()).hexdigest()
+    f= open(str(filename[:10]), "rb")
+    factor = hexlify(f.read())
+    otp = generate_totp(factor.decode())
+    return otp
 def send():
     while True:
         try:
             message = input()
             client.send(message.encode())
-        except:
-            print('Error! Cannot send message to server!')
+            if(message =="/login"):
+                username = input()
+                client.send(username.encode())
+                passw = getpass()
+                client.send(passw.encode())
+                gen = reqOTP(username)
+                print("Your generated OTP sending to server is: ",gen)
+                client.send(gen.encode())
+            elif (message =="/register"):
+                message = input()
+                client.send(message.encode())
+                message = getpass()
+                client.send(message.encode())
+                
+        except Exception as e:
+            print(e)
             client.close()
             break
 
+def generate_totp(secret_key):
+    current_time = int(time.time())
+    time_interval = 60
+    time_steps = current_time // time_interval
+    time_steps_bytes = struct.pack(">Q", time_steps)
+    secret_key_bytes = secret_key.encode("ascii")
+    
+    # Generate an HMAC-SHA1 hash of the time steps using the secret key
+    hmac_hash = hmac.new(secret_key_bytes, time_steps_bytes, hashlib.sha1).digest()
+
+    # Calculate the offset and take last 4-byte for the TOTP code
+    offset = hmac_hash[-1] & 0x0F
+    code_bytes = hmac_hash[offset:offset+4]
+    code = struct.unpack(">I", code_bytes)[0]
+    totp_code= '{0:06d}'.format((code & 0x7FFFFFFF) % 1000000)
+
+    return totp_code
 
 def main():
+    # Verify cert
+    cert = client.getpeercert()
+    if cert:
+        issuer = dict(x[0] for x in cert['issuer'])
+        subject = dict(x[0] for x in cert['subject'])
+        if issuer['commonName'] == "Group6" and subject['commonName'] == "Group6":
+            print("Server certificate verified")
+        else:
+            print("Server certificate verification failed")
+    else:
+        print("Server certificate verification failed")
+
     receive_thread = threading.Thread(target=receive)
     receive_thread.start()
     send_thread = threading.Thread(target=send)
--- Console/server.py
+++ Console/server.py
@@ -3,13 +3,33 @@ import urllib.parse as up
 import psycopg2
 import socket
 import threading
+import binascii
+import hmac
+import hashlib
+import struct
+import time
+import ssl
+from random import getrandbits
+from Crypto.Cipher import AES
 from argon2 import PasswordHasher
 
+
 # Setup connection
+dir_path = os.path.dirname(os.path.abspath(__file__))
+
+# Construct the paths to the certificate and key files
+certfile = os.path.join(dir_path, "server.crt")
+keyfile = os.path.join(dir_path, "server.key")
+
+# Create an SSL context and load the certificate and key files
+context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
+context.load_cert_chain(certfile=certfile, keyfile=keyfile)
+
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR, 1)
-server.bind(("localhost",9999))
+server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+server.bind(("0.0.0.0", 9999))
 server.listen()
+server = context.wrap_socket(server, server_side=True)
 print("Server starting...")
 
 #set up database
@@ -17,37 +37,11 @@ up.uses_netloc.append("rbzkziqg")
 url = up.urlparse("postgres://rbzkziqg:rGJI2QMcTMo7C6GGrC1f1X82FqysVz2H@satao.db.elephantsql.com/rbzkziqg")
 conn = None
 cur = None
-ph = PasswordHasher()
+ph = PasswordHasher() 
+tempOTP =''
 
 def check_permission(client_socket, permission):
     username = client_socket.username
-    #sqlConnection = sqlite3.connect("userdata.db")
-    cursor = sqlConnection.cursor()
-    cursor.execute("""
-        SELECT role FROM userdata
-        WHERE username = ?
-    """, (username,))
-    result = cursor.fetchone()
-    if result is None:
-        return False # user not found
-    role = result[0]
-
-    cursor.execute("""
-        SELECT ? FROM role_permissions
-        WHERE role = ?
-    """, (permission, role))
-    result = cursor.fetchone()
-    if result[0] == 1:
-        return True # permission granted
-    else:
-        return False # permission denied
-    
-def login(client_socket):
-    client_socket.send("Username: ".encode())
-    username = client_socket.recv(1024).decode()
-    client_socket.send("Password: ".encode())
-    password = client_socket.recv(1024).decode()
-
     try:
         conn = psycopg2.connect(database=url.path[1:],
         user=url.username,
@@ -58,13 +52,146 @@ def login(client_socket):
     except Exception as error:
         print(error)
     cur = conn.cursor()
+    cur.execute("""
+        SELECT role FROM userdata
+        WHERE username = ?
+    """, (username,))
+    result = cur.fetchone()
+    if result is None:
+        return False # user not found
+    role = result[0]
+
+    cur.execute("""
+        SELECT ? FROM role_permissions
+        WHERE role = ?
+    """, (permission, role))
+    result = cur.fetchone()
+    if result[0] == 1:
+        return True # permission granted
+    else:
+        return False # permission denied
+    
+def getRoles(username):
+    try:
+        conn = psycopg2.connect(database=url.path[1:],
+        user=url.username,
+        password=url.password,
+        host=url.hostname,
+        port=url.port
+        )
+    except Exception as error:
+        print(error)
+    cur = conn.cursor()
+    cur.execute("SELECT role FROM userInfo WHERE username = %s", [username])
+    result = cur.fetchall()[0][0]
+    if result is None:
+        return False # user not found
+    return result
+
+def generate_totp(secret_key):
+    current_time = int(time.time())
+    time_interval = 60
+    time_steps = current_time // time_interval
+    time_steps_bytes = struct.pack(">Q", time_steps)
+    secret_key_bytes = secret_key.encode("ascii")
+    # Generate an HMAC-SHA1 hash of the time steps using the secret key
+    hmac_hash = hmac.new(secret_key_bytes, time_steps_bytes, hashlib.sha1).digest()
+
+    # Calculate the offset and take last 4-byte for the TOTP code
+    offset = hmac_hash[-1] & 0x0F
+    code_bytes = hmac_hash[offset:offset+4]
+    code = struct.unpack(">I", code_bytes)[0]
+    totp_code= '{0:06d}'.format((code & 0x7FFFFFFF) % 1000000)
+
+    return totp_code
+
+def generateFactor(username):
+    role = getRoles(username)
+    try:
+        conn = psycopg2.connect(database=url.path[1:],
+        user=url.username,
+        password=url.password,
+        host=url.hostname,
+        port=url.port
+        )
+    except Exception as error:
+        print(error)
+    cur = conn.cursor()
+    cur.execute("SELECT delete_user, search_data, insert_data, update_data, delete_data FROM rolePermissions WHERE role = %s", [role])
+    result = cur.fetchall()
+
+    if result:
+        key = binascii.unhexlify("9A1C95B959B9B67EEF032BA0FD0ABC22")
+        permissions = {
+                    'delete_user': result[0][0],
+                    'search_data': result[0][1],
+                    'insert_data': result[0][2],
+                    'update_data': result[0][3],
+                    'delete_data': result[0][4]
+                }
+        permissions_bin = ''.join([bin(value)[2:].zfill(1) for value in permissions.values()])
+        random1 = bin(getrandbits(8))[2:]  
+        random2 = bin(getrandbits(8))[2:]  
+        factor = permissions_bin + random1 + random2
+        enc = AES.new(key, AES.MODE_GCM)
+        ciphertext, tag = enc.encrypt_and_digest(factor.encode())
+        encryptedFactor = ciphertext.hex()
+
+        factor32char = encryptedFactor[:16] + encryptedFactor[-16:]
+         # insert the factor into the database table
+        cur.execute("UPDATE userInfo SET factor = %s WHERE username = %s", [factor32char, username])
+        conn.commit() # commit the transaction to save the changes to the database
+        print(f"Factor saved to database for user {username}: {factor32char}")
+        return factor32char
+    else:
+        print(f"No permissions found for role: {role}")
+
+def reqOtp(username):
+    try:
+        conn = psycopg2.connect(database=url.path[1:],
+        user=url.username,
+        password=url.password,
+        host=url.hostname,
+        port=url.port
+        )
+    except Exception as error:
+        print(error)
+    cur = conn.cursor()
+    cur.execute("SELECT factor FROM userInfo WHERE username = %s", [username])
+    factor = cur.fetchall()[0][0]
+    otp = generate_totp(factor)
+    return otp
+
+def login(client_socket):
+    client_socket.send("Username: ".encode())
+    username = client_socket.recv(1024).decode()
+    password = client_socket.recv(1024).decode()
+
+    client_socket.send("OTP code:".encode())
+    genOTP = reqOtp(username)
+    print(genOTP)
+    otp_code = client_socket.recv(1024).decode()
+    try:
+        conn = psycopg2.connect(database=url.path[1:],
+        user=url.username,
+        password=url.password,
+        host=url.hostname,
+        port=url.port
+        )
+    except Exception as error:
+        print(error)
+
+    cur = conn.cursor()
     cur.execute("SELECT password FROM userInfo WHERE username = %s", [username])
     data = cur.fetchall()[0][0]
     print(str(data))
     try:
         verifyValid = ph.verify(str(data),password)
         print(verifyValid)
-        client_socket.send("Login complete!".encode())
+        if(otp_code == genOTP):
+            client_socket.send("Login complete!".encode())
+        else:
+            client_socket.send("Invalid OTP code")
     except:
         client_socket.send("Login failed!".encode())
     finally:
@@ -72,11 +199,10 @@ def login(client_socket):
 
 
 
-def register(c):
-    c.send("Username: ".encode())
-    username = c.recv(1024).decode()
-    c.send("Password: ".encode())
-    password = c.recv(1024).decode()
+def register(client_socket):
+    client_socket.send("Username: ".encode())
+    username = client_socket.recv(1024).decode()
+    password = client_socket.recv(1024).decode()
     hashpass = ph.hash(password)
     try:
         conn = psycopg2.connect(database=url.path[1:],
@@ -87,11 +213,20 @@ def register(c):
         )
     except Exception as error:
         print(error)
+        
     cur = conn.cursor()
-    cur.execute("INSERT INTO userInfo (username,password,role) VALUES (%s, %s, %s)", (username,hashpass,"normal"))
+    cur.execute("INSERT INTO userInfo (username,password,role) VALUES (%s, %s, %s)", [username,hashpass,"normal"])
     conn.commit()
-    c.send("Register successfully!\n".encode())
-    Menu(c)
+
+    factor = generateFactor(username)
+    cur.execute("UPDATE userInfo SET factor = %s WHERE username = %s", [factor,username])
+    conn.commit()
+    cur.close()
+    conn.close()
+    client_socket.send(("FACTOR:" + username+ "/"+ factor).encode())
+    client_socket.send("Register successfully!\n".encode())
+
+    Menu(client_socket)
 
 
 def Menu(client_socket):
@@ -100,7 +235,7 @@ def Menu(client_socket):
         switch = {
             "/login": login,
             "/register": register,
-            "/exit": exitProgram
+            "/exit": exitProgram,
         }
         handler = switch.get(command, invalidCommand)
         handler(client_socket)
